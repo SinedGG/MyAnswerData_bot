55dc6e97c760e163cf00bde86065e9969a54eb27
Sined <denis2320032@gmail.com>
2021-11-02 18:31:39 +0200

    Hot fix 🔥

--- server.js
+++ server.js
@@ -140,6 +140,6 @@ app.get("/style.css", function (req, res) {
 });
 
 
-var server = app.listen(80, function () {
+var server = app.listen(8080, function () {
   console.log("Listening on port %d", server.address().port);
 });
